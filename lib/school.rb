9fe80ea9b44351f45d5a60ad0d353461936be357
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-30 04:35:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -10,12 +10,14 @@ class School
   def add_student(student_name, grade)
     @student_name = student_name
     @grade = grade
-    if @roster.keys.include?(grade)
-      @roster[grade] << student_name
-    else
-      @roster[grade] = []
-      @roster[grade] << student_name
-    end
+    #if @roster.keys.include?(grade)
+    #  @roster[grade] << student_name
+    #else
+    #  @roster[grade] = []
+    #  @roster[grade] << student_name
+    #end
+    @roster[grade] ||= []
+    @roster[grade] << student_name
   end
   
   def grade(num)
